da93b4f1b9cc9d92d92034e7284cee781d95fd94
Glaucielle Mamede dos Santos <glaucielle.santos@SCGE.PE.GOV.BR>
2019-06-21 12:40:34 -0300

    Meu primeiro commit

---
+++ Bootstrap/test.js
@@ -0,0 +1,18 @@
+ array = [
+     {Nome: 'Glaucielle',ID: 1},
+     {Nome: 'Thiago',ID: 2},
+     {Nome: 'Eduardo',ID: 3},
+];
+
+
+
+array.forEach(usuario => {
+    console.log(usuario);
+});
+
+array.map(usuario => {
+    console.log(usuario);
+});
+
+ //console.log(array[2].Nome);
+
